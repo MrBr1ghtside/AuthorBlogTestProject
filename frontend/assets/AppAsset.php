04ca1d15903a9eaa7d8f927caf31eee7d4fb2dd3
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-07-31 11:51:41 +0500

    add files

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/slick.css',
+        'css/style.css',
+    ];
+    public $js = [
+        'js/jquery.min.js',
+        'js/main.js',
+        'js/slick.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
