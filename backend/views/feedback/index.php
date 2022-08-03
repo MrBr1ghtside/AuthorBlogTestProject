c092eb8cfd8a03f9cf5c32163893a7714c49721d
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-03 11:43:12 +0500

    add feedback model

---
+++ backend/views/feedback/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\FeedbackSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Feedbacks';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="feedback-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Feedback', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'text',
+            'name',
+            'status',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Feedback $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
