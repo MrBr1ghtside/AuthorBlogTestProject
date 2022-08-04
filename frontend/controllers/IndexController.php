e76dc19961a6f750ffd9957510979bfbfcc5c2b5
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-04 09:01:43 +0500

    add another contact form

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+/* Объявляем пространство имен */
+namespace app\controllers;
+
+use Yii;
+
+use yii\web\Controller;
+use app\models\ContactForm;
+use yii\web\Request;
+
+class IndexController extends Controller
+{
+    
+    /* Объявляем функцию */
+    public function actionContact()
+    {
+       /* Для страницы контактов можно использовать свой слой */
+       $this->layout = 'contact';
+       
+        /* Создаем экземпляр класса */
+        $model = new ContactForm();
+        /* получаем данные из формы и запускаем функцию отправки contact, если все хорошо, выводим сообщение об удачной отправке сообщения на почту */
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+            return $this->refresh();
+        /* иначе выводим форму обратной связи */
+        } else {
+            return $this->render('contact', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
