b7d9710d2bb72d3510dfe24ec340682fb59f6ff2
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-12 14:31:09 +0500

    change page content

--- frontend/views/feedbacks/index.php
+++ frontend/views/feedbacks/index.php
@@ -1,13 +1,19 @@
 <section class="fh5co-quotes">
 		<div class="site-container">
 			<div class="about-me-slider">
-				<div>
-					<h2 class="universal-h2 universal-h2-bckg">What People Are Saying</h2>
-					<p>“Pudding croissant cake candy canes fruitcake sweet roll pastry gummies sugar plum. Tart pastry danish soufflé donut bear claw chocolate cake marshmallow chupa chups. Jelly danish gummi bears cake donut powder chocolate cake. Bonbon soufflé lollipop biscuit dragée jelly-o. Wafer pastry pudding tiramisu chocolate bar croissant cake. Pie caramels gummies danish.”</p>
-					<img src="./images/quotes.svg" alt="quotes svg">
-					<h4>David Dixon</h4>
-					<p>Reader</p>
-				</div>
+				<?php foreach ($varInView as $item): ?>
+					<div>
+						<h2 class="universal-h2 universal-h2-bckg">
+							<?php echo $item->title ?>
+						</h2>
+						<p>
+							<?php echo $item->text ?>
+						</p>
+						<img src="./images/quotes.svg" alt="quotes svg">
+						<h4>David Dixon</h4>
+						<p>Reader</p>
+					</div>
+				<?php endforeach ?>
 			</div>
 		</div>
 	</section>
