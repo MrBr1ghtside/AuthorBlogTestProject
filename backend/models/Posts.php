6c8334db4a09b680d0c2cf7e1b93d960fe1f29dc
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-12 14:29:12 +0500

    create MVC posts backend

---
+++ backend/models/Posts.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property int $id
+ * @property string $title
+ * @property int|null $created_at
+ * @property string|null $text
+ */
+class Posts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['created_at'], 'integer'],
+            [['title'], 'string', 'max' => 50],
+            [['text'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'created_at' => 'Created At',
+            'text' => 'Text',
+        ];
+    }
+}
