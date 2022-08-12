72834fd5124f0ebd7694b2e225fcabca3fb5699b
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-12 14:29:59 +0500

    create migration to create table posts

---
+++ console/migrations/m220812_090214_create_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%posts}}`.
+ */
+class m220812_090214_create_posts_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%posts}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(50)->notNull(),
+            'created_at' => $this->integer(),
+            'text' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%posts}}');
+    }
+}
