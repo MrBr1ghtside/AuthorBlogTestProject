ba7e6c5f571d11164dc6b16c66bc5f8cac355ce5
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-08-14 14:04:10 +0500

    add default route in backend

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,6 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log', 'gii'],
+    'defaultRoute' => 'site/index',
     'modules' => [
         'gii'=>[
             'class'=>'yii\gii\Module'
