84b457ac637a0412554849b1eaca512d5f49c8bb
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-09-14 16:47:59 +0500

    changes in contact file

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -8,6 +8,8 @@ use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 use yii\captcha\Captcha;
 use yii\grid\GridView;
+use mirocow\yandexmaps\Map;
+use mirocow\yandexmaps\Canvas;
 
 $this->title = 'Форма обратной связи';
 ?>
@@ -18,7 +20,6 @@ $this->title = 'Форма обратной связи';
             <div class="col-md-8 offset-md-2">
 
                 <h1><?= Html::encode($this->title) ?></h1>
-
                 <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
                     <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
@@ -32,17 +33,11 @@ $this->title = 'Форма обратной связи';
                     <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
                         'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
                     ]) ?>
-
                     <div class="form-group">
                         <?= Html::submitButton('Подтвердить', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                     </div>
+                    
                 <?php ActiveForm::end(); ?>
-            </div>
-                <div class='col-md-8 offset-md-2'>
-                  <iframe
-                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
-                    frameborder="0" style='border:5; height: 400px; width: 745px;'>
-                </iframe>
             </div>
         </div>
 </div>
