3af1558c9905ed9163781549a121555e56d2cc01
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-09-15 12:00:08 +0500

    delete map

--- backend/modules/admin/views/default/index.php
+++ backend/modules/admin/views/default/index.php
@@ -4,13 +4,9 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use mirocow\yandexmaps\Map;
 use mirocow\yandexmaps\Canvas;
-use \msvdev\widgets\mappicker\MapInput;
-use \msvdev\widgets\mappicker\MapYandexAset;
+
 ?>
 
 <div class="admin-default-index">
-<div id="map" style="width: 500px; height: 370px;"></div>
-<input id="coordinates">
 
-<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
 </div>
