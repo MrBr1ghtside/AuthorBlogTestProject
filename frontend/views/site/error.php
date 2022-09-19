d04c831c4d9c1eddbaae27e160bdd53a3950b89b
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-09-19 11:18:05 +0500

    change error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,12 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class='site-container'>
+    <div class="error-page">
+        <img src="./images/book-error.png" alt="error image">
+        <p>
+        Страница, на которую вы пытались попасть, не существует или была удалена.<br> 
+        Пожалуйста, свяжитесь с нами, если вы считаете, что это ошибка сервера. 
+        </p>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
