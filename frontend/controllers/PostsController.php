068483ccd7dfc1673e5355564ebf85c396f72d89
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-09-26 13:14:21 +0500

    add pagination

--- frontend/controllers/PostsController.php
+++ frontend/controllers/PostsController.php
@@ -4,7 +4,7 @@ namespace frontend\controllers;
 
 use Yii;
 use yii\web\Controller;
-
+use yii\data\Pagination;
 use common\models\Posts;
 
 class PostsController extends Controller
@@ -12,8 +12,13 @@ class PostsController extends Controller
 
     public function actionPosts()
     {
-        $array = Posts::getAll();
-        return $this->render('index',['varInView' => $array]);
+        //$array = Posts::find()->all();
+        $query = Posts::find();
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4 ]);
+        $posts = $query->offset($pages->offset)
+            ->limit($pages->limit)
+            ->all();
+        return $this->render('index',['posts' => $posts, 'pages' => $pages]);
     }
 
     public function actionView($slug)
--- frontend/views/posts/index.php
+++ frontend/views/posts/index.php
@@ -1,5 +1,6 @@
 <?php
 use yii\bootstrap4\Html;
+use yii\widgets\LinkPager;
 /** @var yii\web\View $this */
 
 $this->title = 'Posts';
@@ -10,7 +11,7 @@ $this->title = 'Posts';
 			    <h2 class="universal-h2 universal-h2-bckg">My Posts</h2>
 			        <div class="row">
                         <div class="col-md-8 offset-md-2 col-md-push-3">
-                            <?php foreach ($varInView as $item): ?>
+                            <?php foreach ($posts as $item): ?>
                                 <h3><?= Html::a($item['title'], ['posts/view', 'slug' => $item['slug']]) ?></h3>
                                 <p>
                                     <?= date('d.m.Y', $item->created_at) ?>
@@ -19,4 +20,22 @@ $this->title = 'Posts';
                         </div>
                     </div>
 		</div>
-	</section>
+        <div class="col-md-8 offset-md-5 col-md-push-3">
+        <?= LinkPager::widget([
+            'pagination' => $pages,
+            'hideOnSinglePage' => true,
+            'maxButtonCount' => 5,
+            'prevPageLabel' => 'Назад',
+            'nextPageLabel' => 'Далее',
+
+            'options' => [
+                'tag' => 'ul',
+                'class' => 'pagination',
+                'id' => 'pager-container',
+            ],
+
+            'linkOptions' => ['class' => 'paglink'],
+            'activePageCssClass' => 'paglink-active',
+        ]); ?>
+        </div>
+</section>
