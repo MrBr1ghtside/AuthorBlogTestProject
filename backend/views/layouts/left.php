274518b753c9dca24c13ee5aaea1a10ff21bda55
MrBr1ghtside <lazarevoleg2000@mail.ru>
2022-09-30 13:30:18 +0500

    change in admin

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -23,44 +23,16 @@ use yii\helpers\Url;
             </div>
         </div>
 
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    [
-                        'label' => 'Некоторые инструменты',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            [
-                                'label' => 'Редактирование данных',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Настройка информации', 'icon' => 'database', 'url' => ['/settings'],],
-                                    ['label' => 'Авторы', 'icon' => 'database', 'url' => ['/authors'],],
-                                    ['label' => 'Книги', 'icon' => 'database', 'url' => ['/books'],],
-                                    ['label' => 'Отзывы', 'icon' => 'database', 'url' => ['/feedbacks'],],
-                                    ['label' => 'Посты', 'icon' => 'database', 'url' => ['/posts'],],
-                                ],
-                            ],
-                        ],
-                    ],
+                    ['label' => 'Admin Menu', 'options' => ['class' => 'header']],
+                    ['label' => 'Настройка информации', 'icon' => 'database', 'url' => ['/settings'],],
+                    ['label' => 'Авторы', 'icon' => 'database', 'url' => ['/authors'],],
+                    ['label' => 'Книги', 'icon' => 'database', 'url' => ['/books'],],
+                    ['label' => 'Отзывы', 'icon' => 'database', 'url' => ['/feedbacks'],],
+                    ['label' => 'Посты', 'icon' => 'database', 'url' => ['/posts'],],
                 ],
             ]
         ) ?>
